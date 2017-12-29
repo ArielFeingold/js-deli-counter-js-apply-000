efbe1a9610545e2d8bf7bb44fa342bc6e467ff28
Ariel Feingold <feingold.ariel@gmail.com>
2017-12-29 15:11:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-function takeANumber(katzDeli, name){
-katzDeli.push(name)
-return "Welcome, " + name + ". You are number" + katzDeli[i] + " in line."
+function takeANumber(katzDeliLine, name){
+var placeInLine = katzDeliLine.length + 1;
+katzDeliLine.push(name)
+return "Welcome, " + name + ". You are number " + placeInLine + " in line."
+};
+
+function nowServing(katzDeliLine){
+	if(katzDeliLine.length === 0){
+		return "There is nobody waiting to be served!"
+	} else{
+		var nextInLine = katzDeliLine[0];
+		var next = katzDeliLine.slice(0, 1)
+		katzDeliLine.shift()
+		return "Currently serving " + next + "."
+	}
+}
